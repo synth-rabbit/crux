63a99ccca8de5679f60c4a1a9a107573f0a6dc7d
Joshua 'PANDA' Edwards <josh.dean.edwards@gmail.com>
2025-06-06 16:58:11 -0500

    feat(reactivity): add simple store implementation

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,4 +1,6 @@
 export { createSignal } from './signal';
 export { effect, onCleanup } from './effect';
 export { computed } from './computed';
+export { createStore } from './store';
+export type { Store } from './store';
 export type { Accessor, Setter, ReactiveEffect, Signal } from './types';
